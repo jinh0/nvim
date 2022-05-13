9fd4e937f504bf3a53e30384e72255badad87408
Jinho <yoonjinho0318@gmail.com>
2022-05-13 10:16:07 -0400

    Add nvim-cmp.

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,63 @@
+local cmp = require'cmp'
+local luasnip = require 'luasnip'
+
+-- Global setup.
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+      -- -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = "nvim_lua" },
+    { name = "vsnip" },
+    { name = "path" },
+    { name = "buffer" },
+    { name = "nvim_lsp_signature_help" },
+    -- { name = 'luasnip' },
+  }
+  -- },  sources = cmp.config.sources({
+    -- { name = 'nvim_lsp' },
+  -- }, {
+    -- { name = "nvim_lua" },
+    -- { name = "vsnip" },
+    -- { name = "path" },
+    -- { name = "buffer" },
+    -- { name = "nvim_lsp_signature_help" },
+  -- })
+})
