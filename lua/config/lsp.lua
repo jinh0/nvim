6a701ab32be3c037782e8023a8aed8d09026401b
Jinho Yoon <yoonjinho0318@gmail.com>
2023-07-15 11:51:40 -0400

    feat: add more lsp keymaps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,7 +2,8 @@ local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
-  vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, {buffer = true})
+  vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, {buffer = bufnr})
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {buffer = bufnr})
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
