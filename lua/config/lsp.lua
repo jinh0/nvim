3986718816af967b8548d45f14f66719d8390141
Jinho Yoon <yoonjinho0318@gmail.com>
2023-09-01 21:16:00 -0400

    feat: add haskell

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -11,6 +11,10 @@ require'lspconfig'.ocamllsp.setup{
   on_attach=require'virtualtypes'.on_attach
 }
 
+require 'lspconfig'.hls.setup {
+  cmd = { "haskell-language-server-wrapper", "--lsp" }
+}
+
 lsp.setup()
 
 local cmp = require('cmp')
