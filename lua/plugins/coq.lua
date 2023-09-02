4ad254586480b83c7a77ccdd8f8d760a1e684fbe
Jinho Yoon <yoonjinho0318@gmail.com>
2023-09-01 21:16:07 -0400

    feat: setup coq

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -1,8 +1,10 @@
 return {
   'whonore/Coqtail',
   config = function ()
-    vim.keymap.set('n', '<a-j>', ':CoqNext<cr>', {})
-    vim.keymap.set('n', '<a-k>', ':CoqUndo<cr>', {})
+    vim.g.coqtail_nomap = 1
+
+    vim.keymap.set('n', 'J', ':CoqNext<cr>', {})
+    vim.keymap.set('n', 'K', ':CoqUndo<cr>', {})
     vim.keymap.set('n', '<a-l>', ':CoqToLine<cr>', {})
   end
 }
